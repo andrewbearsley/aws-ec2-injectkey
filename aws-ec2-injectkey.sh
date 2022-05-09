45652790115c0c0716d851e49847465665af462f
Andrew Bearsley <8905436+andrewbearsley@users.noreply.github.com>
2022-05-09 21:09:44 +1000

    Create aws-ec2-injectkey.sh

---
+++ aws-ec2-injectkey.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Inject public key into AWS ECS Linux running AWS Systems Manager, then SSH into machine
+
+# AWS EC2 Details
+export INSTANCEID=i-00c6e55a5f0271c62
+export INSTANCEIP=13.239.26.48
+export INSTANCEOSUSER=ubuntu
+export AWSREGION=ap-southeast-2
+export AWSPROFILE=default
+
+# Create key pair
+export PEMKEY=MyKeyPair
+aws ec2 create-key-pair --key-name $PEMKEY --profile $AWSPROFILE --query 'KeyMaterial' --output text > $PEMKEY.pem
+chmod 400 $PEMKEY.pem
+ssh-keygen -y -f $PEMKEY.pem > $PEMKEY.pub
+
+# Inject public key to EC2
+aws ec2-instance-connect send-ssh-public-key --instance-id $INSTANCEID --instance-os-user $INSTANCEOSUSER --region $AWSREGION --profile $AWSPROFILE --ssh-public-key "$(cat $PEMKEY.pub)" | jq
+
+# Connect to EC2
+ssh -i "$PEMKEY.pem" $INSTANCEOSUSER@$INSTANCEIP
+# Currently this fails with "ubuntu@13.239.26.48: Permission denied (publickey).""
+
+# Cleanup - delete key pair
+# aws ec2 delete-key-pair --key-name MyKeyPair --profile $AWSPROFILE 
+# yes | rm $PEMKEY.*
